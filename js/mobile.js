409ea8ecdd439c706c75409a1f2e771c40f9b755
Tinsson <695724046@qq.com>
2017-05-12 18:41:58 +0800

    tickets

---
+++ js/mobile.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var clientWid = document.body.clientWidth,
+	    doc = document.documentElement;;
+	doc.style.fontSize = clientWid / 5 + "px";
+	$(window).resize(function(){
+		var clientWid = document.body.clientWidth,
+		    doc = document.documentElement;
+		doc.style.fontSize = clientWid / 5 + "px";
+	})
+
+
+})
